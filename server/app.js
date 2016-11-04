324250292ff87ed7a3eb418748c02521ab4381eb
David Blumer <david.blumer@socialbit.de>
2016-11-04 23:52:44 +0100

    adds basic user-movement and player colors

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
-const app  = require('express')();
-const _    = require('lodash');
+const app = require('express')();
+const _ = require('lodash');
 const http = require('http').Server(app);
-const io   = require('socket.io')(http);
+const io = require('socket.io')(http);
 
-var server_port       = process.env.PORT || 1338;
+var server_port = process.env.PORT || 1338;
 
 // Header etc.
 app.use(function (req, res, next) {
@@ -12,47 +12,59 @@ app.use(function (req, res, next) {
 });
 
 
-
 var clients = [];
+var colors =  ['green', 'blue', 'red', 'yellow', 'beige'];
 var map = [];
 
-io.on('user.session.connect', function(socket, name){
-    clients.push({id: socket.id, name: name});
+io.on('user.session.connect', function (socket, data) {
+    var newLocation = {
+        x: 0,
+        y: 0
+    };
+
+
+    var user = {
+        id: socket.id,
+        name: data.name,
+        location: newLocation,
+        color: colors[clients.length]
+    };
+    
+    clients.push(user);
 });
 
+/**
+ * User-Movement
+ */
+io.on('user.movement', function (socket, data) {
+    var user = _.find(clients, ['id', userId]);
 
+    if (data.left) {
+        --user.location.x
+    }
+    if (data.right) {
+        ++user.location.x
+    }
+    if (data.up) {
+        ++user.location.y
+    }
+    if (data.down) {
+        --user.location.y
+    }
 
+    io.emit('users.location.change', user);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Routes
-app.get('/', function(req, res) {
+/**
+ * Routes
+ */
+app.get('/', function (req, res) {
     res.send('Hello orld!');
 });
 
-// server listens in on port
+/**
+ * Running
+ */
 http.listen(server_port, function () {
-    console.log( "Server running on port: " + server_port );
+    console.log("Server running on port: " + server_port);
 });
-
-
-
-function executeMovement(userId, movement) {
-    var user = _.find(clients, ['id', userId]);
-
-
-    for(var i = 0; i < movement.length; i++) {
-
-    }
-}
