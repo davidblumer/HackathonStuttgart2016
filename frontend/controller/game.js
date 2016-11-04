2915ec3c79f0d23b427cf70a8f3971b88cd6f745
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-11-04 22:56:26 +0100

    Added socket.io for the frontend and fixed some bugs

--- frontend/controller/game.js
+++ frontend/controller/game.js
@@ -1,3 +1,111 @@
+/**
+ * Hackathon Stuttgart Frontend Code
+ *
+ * @see https://github.com/SocialbitGmbH/HackathonStuttgart2016/tree/develop
+ */
+
+var logPrefix = 'HACKSTGT16: ';
+
+/**
+ *
+ */
+function gameCreate()
+{
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.stage.backgroundColor = '#CCCCCC';
+
+}
+
+/**
+*
+*/
+function gamePreload()
+{
+    // TODO
+
+}
+
+/**
+ *
+ */
+function gameRender()
+{
+    // TODO
+}
+
+/**
+ *
+ */
+function gameUpdate()
+{
+    // TODO
+}
+
+/**
+ * @param target
+ * @returns {*}
+ */
+function getServerAddress (target)
+{
+    if (target == 'josh')  return '192.168.2.135:1338';
+    if (target == 'david') return '192.168.2.120:1338';
+
+    return 'localhost:1338';
+}
+
+/**
+ * @returns {{force new connection: boolean}}
+ */
+function getServerConnectionOptions ()
+{
+    return {
+        'force new connection': true
+    };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var socket = io.connect(getServerAddress('david'), getServerConnectionOptions());
+
+
+socket.on('connect', function ()
+{
+    console.log(logPrefix + 'socket connected');
+
+
+});
+
+socket.on('disconnect', function ()
+{
+    console.log(logPrefix + 'socket disconnected');
+
+
+});
+
+socket.on('error', function()
+{
+    console.log(logPrefix + 'socket error');
+
+
+});
+
+
 
 
 
@@ -7,48 +115,11 @@ var game = new Phaser.Game(
     Phaser.CANVAS,
     'hackathonStuttgart',
     {
-        preload: preload,
-        create:  create,
-        update:  update,
-        render:  render
+        preload: gamePreload,
+        create:  gameCreate,
+        update:  gameUpdate,
+        render:  gameRender
     }
 );
 
 
-function preload()
-{
-    // TODO
-
-}
-
-
-
-
-function create()
-{
-
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    game.stage.backgroundColor = '#CCCCCC';
-
-
-
-
-
-}
-
-function update()
-{
-    // TODO
-
-
-
-}
-
-function render()
-{
-    // TODO
-
-
-
-}
