e8f11c5f29d2327e5b74fac54fe97c935ee878a4
Joshua Schumacher <joshua.schumacher@socialbit.de>
2016-11-05 23:16:29 +0100

    MAde spawn random

--- server/app.js
+++ server/app.js
@@ -129,8 +129,8 @@ function generateLocation() {
     //     y: Math.round(Math.random() * (map.tilesets[0].imageheight - 0) + 0)
     // };
     return {
-        x: 34,
-        y: 26
+        x: Math.floor(Math.random() * 42) + 34,
+        y: Math.floor(Math.random() * 27) + 26
     };
 }
 
