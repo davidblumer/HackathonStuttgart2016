fcdd97fe2dfee1647deee614288a3f5fe4b23955
David Blumer <david.blumer@socialbit.de>
2016-11-05 18:02:26 +0100

    cache testing

--- server/app.js
+++ server/app.js
@@ -4,49 +4,43 @@ var map = require('./map.json');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-
 /*
-var Jimp = require("jimp");
+ var Jimp = require("jimp");
 
-Jimp.read("../frontend/assets/images/tilemaps/map.png", function (err, sourceMap)
-{
-    var tileSize  = 16;
-    var spaceSize = 1;
-    var tilesX    = (sourceMap.bitmap.width + 1) / (tileSize + spaceSize);
-    var tilesY    = (sourceMap.bitmap.height + 1) / (tileSize + spaceSize);
+ Jimp.read("../frontend/assets/images/tilemaps/map.png", function (err, sourceMap)
+ {
+ var tileSize  = 16;
+ var spaceSize = 1;
+ var tilesX    = (sourceMap.bitmap.width + 1) / (tileSize + spaceSize);
+ var tilesY    = (sourceMap.bitmap.height + 1) / (tileSize + spaceSize);
 
-    console.log('Fixing image, tiles:', tilesX, tilesY);
+ console.log('Fixing image, tiles:', tilesX, tilesY);
 
-    var newSize = {
-        height: tileSize * tilesY,
-        width:  tileSize * tilesX
-    };
+ var newSize = {
+ height: tileSize * tilesY,
+ width:  tileSize * tilesX
+ };
 
-    var targetImage = new Jimp(newSize.width, newSize.height, function (err, newImage)
-    {
-        for (var x = 0; x < tilesX; ++x)
-        {
-            for (var y = 0; y < tilesY; ++y)
-            {
-                newImage.blit(
-                    sourceMap,
-                    x * tileSize,
-                    y * tileSize,
-                    (x * (tileSize + spaceSize)),
-                    (y * (tileSize + spaceSize)),
-                    tileSize,
-                    tileSize);
-            }
-        }
+ var targetImage = new Jimp(newSize.width, newSize.height, function (err, newImage)
+ {
+ for (var x = 0; x < tilesX; ++x)
+ {
+ for (var y = 0; y < tilesY; ++y)
+ {
+ newImage.blit(
+ sourceMap,
+ x * tileSize,
+ y * tileSize,
+ (x * (tileSize + spaceSize)),
+ (y * (tileSize + spaceSize)),
+ tileSize,
+ tileSize);
+ }
+ }
 
-        newImage.write('../frontend/assets/images/tilemaps/mapNoSpace.png')
-    });
-});*/
-
-
-
-
-
+ newImage.write('../frontend/assets/images/tilemaps/mapNoSpace.png')
+ });
+ });*/
 
 var server_port = process.env.PORT || 1338;
 
@@ -64,35 +58,43 @@ var colors = ['green', 'blue', 'red', 'yellow', 'beige'];
  * Map
  */
 
-var allowedTerrain = {
-    dirt: [512, 513, 514, 568, 569, 570, 624, 625, 626],
-    water: [3, 4, 5, 59, 60, 61, 115, 116, 117],
-    gravel: [1184, 1185, 1186, 1240, 1241, 1242, 1296, 1297, 1298],
-    grass: [843, 844, 845, 899, 900, 901, 955, 956, 957],
-    stone: [848, 849, 850, 904, 905, 906, 960, 961, 962],
-    rails: [1050, 1051, 1052, 1106, 1107, 1108]
-};
+// var allowedTerrain = {
+//     dirt: [512, 513, 514, 568, 569, 570, 624, 625, 626],
+//     water: [3, 4, 5, 59, 60, 61, 115, 116, 117],
+//     gravel: [1184, 1185, 1186, 1240, 1241, 1242, 1296, 1297, 1298],
+//     grass: [843, 844, 845, 899, 900, 901, 955, 956, 957],
+//     stone: [848, 849, 850, 904, 905, 906, 960, 961, 962],
+//     rails: [1050, 1051, 1052, 1106, 1107, 1108]
+// };
+
+var allowedTerrain = [707, 708, 709, 710, 711, 712, 889, 1164, 1952, 77, 76, 899, 715, 744, 745, 746, 747, 748, 749];
 var cache = {collision: [], walkable: []};
 
 function isMovementAllowed(location) {
 
-    console.log(map);
-    // if (_.includes(cache.collision, location)) {
-    //     return false;
-    // }
-    // else if (_.includes(cache.walkable, location)) {
-    //     return true
-    // }
-    // for (var i = 0; i < map.layers.length; i++) {
-    //     if (_.includes(allowedTerrain, map.layers[i].data[newDestination])) {
-    //         cache.walkable.push(newDestination);
-    //         return true;
-    //     } else {
-    //         cache.collision.push(newDestination);
-    //         return false;
-    //     }
-    // }
-    return true
+    if (_.includes(cache.collision, location)) {
+        return false;
+    }
+    else if (_.includes(cache.walkable, location)) {
+        return true
+    }
+
+    _.forEach(map.layers, function (layer) {
+        var locationInArray = ((location.y - 1) * map.width) + location.x;
+        var activeTile = layer.data[Math.round(locationInArray / 16)];
+
+        console.log(location);
+        if (activeTile > 0) {
+            console.log(activeTile);
+            if (_.includes(allowedTerrain, activeTile)) {
+                cache.walkable.push(location);
+                return true
+            } else {
+                cache.collision.push(location);
+                return false;
+            }
+        }
+    });
 }
 
 function generateLocation() {
@@ -101,8 +103,8 @@ function generateLocation() {
     //     y: Math.round(Math.random() * (map.tilesets[0].imageheight - 0) + 0)
     // };
     return {
-        x: 50,
-        y: 50
+        x: 16,
+        y: 16
     };
 }
 
@@ -182,9 +184,9 @@ io.on('connection', function (socket) {
                 user.direction = 'down';
             }
 
-            // if(isMovementAllowed(newLocation)) {
+            if (isMovementAllowed(newLocation)) {
                 user.location = newLocation;
-            // }
+            }
             user.lastMovement = now;
             io.emit('user.location.change', socket.id, user);
         }
@@ -214,3 +216,28 @@ app.get('/', function (req, res) {
 http.listen(server_port, function () {
     console.log("Server running on port: " + server_port);
 });
+
+function buildCache() {
+    var y = 0;
+    var mapWidth = map.tilesets[0].imagewidth;
+
+    _.forEach(map.layers, function (layer) {
+        for (var i = 0; i < layer.data.length; i++) {
+            var terrainId = layer.data[i];
+            if (terrainId > 0 && !_.includes(allowedTerrain, terrainId)) {
+                var index = cache.collision.indexOf(i);
+                if (index == -1) {
+                    var y = Math.floor(i / mapWidth);
+                    var x = mapWidth - y;
+                    var location = {
+                        x: x,
+                        y: y
+                    };
+                    console.log(i, location, layer.data[i]);
+                    cache.collision.push(i);
+                }
+            }
+        }
+    });
+}
+buildCache();
