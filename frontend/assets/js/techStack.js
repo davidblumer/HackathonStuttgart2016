dcedc38806ab11274168dff6ce67a8e7a579d1fb
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2016-11-05 20:29:08 +0100

    Add tech stack stuff

---
+++ frontend/assets/js/techStack.js
@@ -0,0 +1,15 @@
+function showTechStack ()
+{
+    var string = ['<table>'];
+
+
+    string.push('<tr><td>API proxies</td><td>php</td>');
+    string.push('<tr><td>Game framework</td><td>phaser.js</td>');
+    string.push('<tr><td>Multiplayer</td><td>socket.io</td>');
+    string.push('<tr><td>IDE</td><td>PHPStorm</td>');
+    string.push('<tr><td>Package managers</td><td>npm, bower</td>');
+
+    string.push('</table>');
+
+    showQuest(string.join(''), 'tech');
+}
